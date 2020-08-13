6247df99aa52eeceed5e2dacae3fde8d997fab68
A.S.Sibi <45584388+assibi039@users.noreply.github.com>
2020-08-13 23:27:18 +0530

    Update hfb.py

--- hfb.py
+++ hfb.py
@@ -12,7 +12,7 @@ elem.send_keys('URK17CS039')
 
 elem = driver.find_element_by_xpath('/html/body/form/div[3]/div[3]/div/div[4]/div[3]/div/div[2]/div/div/div/div[2]/div[1]/div/table/tbody/tr/td/div[2]/div/div[2]/input')
 
-elem.send_keys('#HBprince700')
+elem.send_keys('password')
 
 elem = driver.find_element_by_xpath('/html/body/form/div[3]/div[3]/div/div[4]/div[3]/div/div[2]/div/div/div/div[2]/div[1]/div/table/tbody/tr/td/div[3]/div/input')
 
